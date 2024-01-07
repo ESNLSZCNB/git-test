ea8a09a457889b0ec7a1fb6a17f874ad36f556d2
messi <lszcnb@outlook.com>
2024-01-07 17:43:09 +0800

    first commit

---
+++ src/main/java/com/lsz/Main.java
@@ -0,0 +1,13 @@
+package com.lsz;
+
+/**
+ * @version 1.0
+ * @Author 卢守征
+ * @Date ${DATE} ${TIME}
+ * @注释
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/lsz/Test.java
@@ -0,0 +1,13 @@
+package com.lsz;
+
+/**
+ * @version 1.0
+ * @Author 卢守征
+ * @Date 2024/1/7 17:37
+ * @注释
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
